d300c3a4fb2fd8469c2d7aaea6872170bb1b4e20
Shaun Ellis <shaun@sdellis.com>
2022-04-17 11:18:58 -0400

    audio player init commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <header class="heading">
+    	<h1>Those Looks - Cults Near Me (Preview)</h1>
+    	<p>Velvet wave from the wilds of New Jersey.</p>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+
+
+</style>
